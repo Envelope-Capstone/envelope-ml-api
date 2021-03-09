87e84b55f64d9756f8dedc1e188b88363d894623
jsamyn <joseph.j.samyn@gmail.com>
2021-03-08 21:09:01 -0700

    Added requests package to requirements.txt and finished the require jwt decorator

--- api/decorators/require_jwt.py
+++ api/decorators/require_jwt.py
@@ -1,10 +1,22 @@
 from functools import wraps
-from flask import request
+from flask import request, abort
+import json
+import requests
+
+jwt_validate_url = 'https://envelope-auth-api.azurewebsites.net/token/validate'
+jwt_refresh_url = 'https://envelope-auth-api.azurewebsites.net/token/refresh'
 
 def require_jwt(controller_func):
     @wraps(controller_func)
     def wrap(*args, **kwargs):
+        # Get auth token and validate it
         auth_token = request.headers['Authorization']
-        print(auth_token)
+        body = {'access_token': auth_token}
+        validation_resp = requests.post(jwt_validate_url, json=body, headers={'Content-Type':'application/json'})
+        
+        # Check if status code is 401 and if so end request
+        if validation_resp.status_code == 401:
+            return {'message': 'User is unauthorized to access this endpoint.'}, 401
+
         return controller_func(*args, **kwargs)
     return wrap
